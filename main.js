76550b9d943898b76fb5802f6215056de960f07a
ebukizy1 <ebukizy1@gmail.com>
2024-05-09 06:23:03 +0100

    update

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const morgan = require("morgan");
 
 const app = express();
 
-app.use(cors());
+app.use(cors("*"));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(morgan("common"));
